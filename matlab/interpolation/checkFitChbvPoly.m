d5161b41d8ed1f145016bf96c0fab9e4fcea5766
PeterC <petercalifano.gs@gmail.com>
2025-08-06 10:51:55 +0200

    Add error tolerance check and error throw (enabled by default)

--- matlab/interpolation/checkFitChbvPoly.m
+++ matlab/interpolation/checkFitChbvPoly.m
@@ -1,11 +1,12 @@
 function [strfitStats, dChbvInterpVector] = checkFitChbvPoly(ui32PolyDeg, ...
-    dInterpDomain, ...
-    dChbvCoeffs, ...
-    dDataMatrix, ...
-    dDomainLB, ...
-    dDomainUB, ...
-    bIS_ATT_QUAT, ...
-    dSwitchIntervals)
+                                                            dInterpDomain, ...
+                                                            dChbvCoeffs, ...
+                                                            dDataMatrix, ...
+                                                            dDomainLB, ...
+                                                            dDomainUB, ...
+                                                            bIS_ATT_QUAT, ...
+                                                            dSwitchIntervals, ...
+                                                            bEnableErrorThrow) %#codegen
 arguments
     ui32PolyDeg         (1, 1) uint32
     dInterpDomain       (:, 1) double
@@ -15,10 +16,18 @@ arguments
     dDomainUB           (1, 1) double
     bIS_ATT_QUAT        (1, 1) logical
     dSwitchIntervals    (:, :) double = []
+    bEnableErrorThrow   (1,1) logical {islogical, isscalar} = true
 end
 %% PROTOTYPE
-% [strfitStats] = checkFitChbvPoly(ui8PolyDeg, dInterpDomain, ...
-%     dDataMatrix, dDomainLB, dDomainUB)
+% [strfitStats, dChbvInterpVector] = checkFitChbvPoly(ui32PolyDeg, ...
+%                                                     dInterpDomain, ...
+%                                                     dChbvCoeffs, ...
+%                                                     dDataMatrix, ...
+%                                                     dDomainLB, ...
+%                                                     dDomainUB, ...
+%                                                     bIS_ATT_QUAT, ...
+%                                                     dSwitchIntervals, ...
+%                                                     bEnableErrorThrow) %#codegen
 % -------------------------------------------------------------------------------------------------------------
 %% DESCRIPTION
 % Function performing fitting check for Chebyshev interpolation functions. It uses randomly picked input
@@ -26,14 +35,15 @@ end
 % the dot product to evaluate quaternions instead of subtraction.
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
-% ui8PolyDeg    (1, 1) uint8
-% dInterpDomain (:, 1) double
-% dChbvCoeffs   (:, 1) double
-% dDataMatrix   (:, :) double
-% dDomainLB     (1, 1) double
-% dDomainUB     (1, 1) double
-% bIS_ATT_QUAT  (1, 1) logical
-% dswitchIntervals (:, :) double = []
+% ui32PolyDeg         (1, 1) uint32
+% dInterpDomain       (:, 1) double
+% dChbvCoeffs         (:, 1) double
+% dDataMatrix         (:, :) double
+% dDomainLB           (1, 1) double
+% dDomainUB           (1, 1) double
+% bIS_ATT_QUAT        (1, 1) logical
+% dSwitchIntervals    (:, :) double = []
+% bEnableErrorThrow   (1,1) logical {islogical, isscalar} = true
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
 % strfitStats
@@ -41,6 +51,7 @@ end
 % -------------------------------------------------------------------------------------------------------------
 %% CHANGELOG
 % 08-05-2024        Pietro Califano         Function adapted from testing script.
+% 06-0
 % -------------------------------------------------------------------------------------------------------------
 %% DEPENDENCIES
 % [-]
@@ -105,28 +116,39 @@ if bIS_ATT_QUAT
     fprintf('Max absolute difference of (q1-dot-q2 - 1): %4.4g [-]\n', strfitStats.dMaxAbsErr);
     fprintf('Average absolute difference of (q1-dot-q2 - 1): %4.4g [-]\n', strfitStats.dAvgAbsErr);
 
+    if bEnableErrorThrow
+        assert( all([strfitStats.dAvgAbsErr, strfitStats.dMaxAbsErr] < 1E-3), ...
+            ['ERROR: fitting validation failed to meet tolerances for attitude quaternion. ' ...
+            'Found distance greater than 1E-3 at sampling nodes.']);
+    end
 else
-    strfitStats.absErrVec = abs(dChbvInterpVector - TestPoints_Labels);
-    strfitStats.relErrVec = strfitStats.absErrVec./vecnorm(TestPoints_Labels, 2, 1);
+    strfitStats.dAbsErrVec = abs(dChbvInterpVector - TestPoints_Labels);
+    strfitStats.dRelErrVec = strfitStats.dAbsErrVec./vecnorm(TestPoints_Labels, 2, 1);
 
-    strfitStats.maxAbsErr = max(strfitStats.absErrVec, [], 'all');
-    strfitStats.avgAbsErr = mean(strfitStats.absErrVec, 2);
+    strfitStats.dMaxAbsErr = max(strfitStats.dAbsErrVec, [], 'all');
+    strfitStats.dAvgAbsErr = mean(strfitStats.dAbsErrVec, 2);
 
-    strfitStats.maxRelErr = 100*max(strfitStats.relErrVec, [], 'all');
-    strfitStats.avgRelErr = 100*mean(strfitStats.relErrVec, 2);
+    strfitStats.dMaxRelErr = 100*max(strfitStats.dRelErrVec, [], 'all');
+    strfitStats.dAvgRelErr = 100*mean(strfitStats.dRelErrVec, 2);
 
     % Printing
-    fprintf('Max absolute error: %4.4g [-]\n', strfitStats.maxAbsErr);
+    fprintf('Max absolute error: %4.4g [-]\n', strfitStats.dMaxAbsErr);
 
     % Print average absolute errors per component on one line (wraps for large N)
     fprintf('Average absolute errors (per component): ');
-    fprintf('%4.4g ', strfitStats.avgAbsErr);
+    fprintf('%4.4g ', strfitStats.dAvgAbsErr);
     fprintf('[-]\n');
 
-    fprintf('\nMax relative error: %4.4g [%%]\n', strfitStats.maxRelErr);
+    fprintf('\nMax relative error: %4.4g [%%]\n', strfitStats.dMaxRelErr);
     fprintf('Average relative errors (per component): ');
-    fprintf('%4.4g ', strfitStats.avgRelErr);
+    fprintf('%4.4g ', strfitStats.dAvgRelErr);
     fprintf('[%%]\n');
+
+    if bEnableErrorThrow
+        assert( all([strfitStats.dMaxRelErr, strfitStats.dAvgRelErr] < 1E-1), ...
+            ['ERROR: fitting validation failed to meet relative tolerances. ' ...
+            'Found relative error greater than 0.1% at sampling nodes.']);
+    end
 end
 
 
