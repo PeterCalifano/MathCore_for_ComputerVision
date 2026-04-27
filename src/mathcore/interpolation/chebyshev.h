14c171cf50a2ca19b77ccba8e49ecc90922e27c0
PeterC <petercalifano.gs@gmail.com>
2026-04-27 11:14:01 +0200

    Attempt to fix CI build failure

--- src/mathcore/interpolation/chebyshev.h
+++ src/mathcore/interpolation/chebyshev.h
@@ -49,7 +49,7 @@ namespace mathcore
         /// Fits the Chebyshev coefficient matrix for the currently configured domain.
         void fitCoefficientMatrix(const matrix_type &data_matrix)
         {
-            this->checkDataMatrixValidity(data_matrix);
+            (void)this->checkDataMatrixValidity(data_matrix);
 
             const auto scaled_domain = base_type::scaleInterpolationDomain(this->interpolation_domain_);
             this->coefficient_matrix_.resize(this->num_components_, this->polynomial_degree_ + 1);
