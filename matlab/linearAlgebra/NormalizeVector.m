7f283f74b93922bb424921f0118fd014489666cf
PeterC <petercalifano.gs@gmail.com>
2026-07-09 15:55:50 +0200

    Implement NormalizeVector (norm-safe)

--- matlab/linearAlgebra/NormalizeVector.m
+++ matlab/linearAlgebra/NormalizeVector.m
@@ -1,18 +1,23 @@
 function dUnitVector = NormalizeVector(dVector, dMinNorm)%#codegen
 arguments
-    dVector  (3,1) 
-    dMinNorm (1,1) {mustBePositive, coder.mustBeConst} = eps
+    dVector  (3,1) double
+    dMinNorm (1,1) double {mustBePositive} = eps
 end
 % Normalizes a vector and with zero norm safe division.
 
 coder.inline('always');
-
 if coder.const(nargin < 2 || isempty(dMinNorm))
     dMinNorm = eps;
 end
 
-dNorm = max(norm(dVector), dMinNorm);
-assert(isfinite(dNorm) && dNorm > dMinNorm);
-dUnitVector = dVector ./ dNorm;
+dNorm = norm(dVector);
+if dNorm < dMinNorm
+    dUnitVector = zeros(3,1);
+    if coder.target('MATLAB')
+        warning('NormalizeVector:ZeroNorm', 'Vector norm is below minimum threshold. Returning zero vector.');
+    end
+else
+    dUnitVector = dVector ./ dNorm;
+end
 
 end
